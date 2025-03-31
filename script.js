3e7377cfdf9b3a2c48f7a04a6acf4ca2ac719e63
Salem Alhaddad <85304055+yttr1um@users.noreply.github.com>
2025-03-31 21:40:58 +0400

    Add function that takes in human choice
    added the function that takes in the human choice and makes sure its valid and returns a case insensitive value.

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
+//global variables
 const humanScore = 0;
 const computerScore = 0;
 
+const choices = ['rock', 'paper', 'scissors'];
+
+//functions
 function getComputerChoice() {
-    choices = ['rock', 'paper', 'scissors'];
-    randomInt = Math.floor(Math.random()*3);
+    let randomInt = Math.floor(Math.random()*3);
     return choices[randomInt];
 }
+
+function getHumanChoice() {
+    let choice = prompt("Enter your move: ");
+
+    //check if choice is part of the choices
+    //returns the choice in lowercase to make it case insensitive
+    return (choices.includes(choice.toLowerCase())) ? choice.toLowerCase() : "invalid input";
+}
+
+console.log(getComputerChoice());
+console.log(getHumanChoice());
+
+// function playRound(humanChoice, computerChoice) {
+//     //conditions
+// }
+
+// const humanSelection = getHumanChoice();
+// const computerSelection = getComputerChoice();
+
+// playRound(humanSelection, computerSelection);
