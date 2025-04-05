7a3d71839c93938b8c204c11fe1ff60514608dd2
Salem Alhaddad <85304055+yttr1um@users.noreply.github.com>
2025-04-05 09:05:41 +0400

    added GUI

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const choices = ['rock', 'paper', 'scissors'];
+const choices = ['🪨', '📄', '✂️'];
 
 //functions
 function getComputerChoice() {
@@ -6,74 +6,101 @@ function getComputerChoice() {
     return choices[randomInt];
 }
 
-function getHumanChoice() {
-    let choice = prompt("Enter your move: ");
+let buttons = document.querySelector("#playing-buttons");
+let showRound = document.querySelector("#round")
+let mainContainer = document.querySelector("#container")
+let message = document.querySelector("#message");
+let scores = document.querySelector("#scores");
+let gamePlay = document.querySelector("#game");
 
-    //check if choice is part of the choices
-    //returns the choice in lowercase to make it case insensitive
-    return (choices.includes(choice.toLowerCase())) ? choice.toLowerCase() : underfined;
-}
 
 function playRound(humanChoice, computerChoice) {
 
+    if (humanChoice === computerChoice) {
+        message.textContent = "It's a tie!";
+    }
+
     //the game logic
-    if (humanChoice == 'rock') {
-        if (computerChoice == 'paper') {
-            console.log("paper beats rock")
+    if (humanChoice == '🪨') {
+        if (computerChoice == '📄') {
+            message.textContent = "paper beats rock";
             return 'c';
-        } if (computerChoice == 'scissors') {
-            console.log('rock beats scissors');
+        } if (computerChoice == '✂️') {
+            message.textContent = 'rock beats scissors';
             return 'h';
         }
     }
 
-    if (humanChoice == 'paper') {
-        if (computerChoice == 'rock') {
-            console.log('paper beats rock');
+    if (humanChoice == '📄') {
+        if (computerChoice == '🪨') {
+            message.textContent = 'paper beats rock';
             return 'h';
-        } if (computerChoice == 'scissors') {
-            console.log('scissors beats paper');
+        } if (computerChoice == '✂️') {
+            message.textContent = 'scissors beats paper';
             return 'c';
         }
     }
 
-    if (humanChoice == 'scissors') {
-        if (computerChoice == 'rock') {
-            console.log('rock beats scissors')
+    if (humanChoice == '✂️') {
+        if (computerChoice == '🪨') {
+            message.textContent = 'rock beats scissors';
             return 'c';
-        } if (computerChoice == 'paper') {
-            console.log('scissors beats paper');
+        } if (computerChoice == '📄') {
+            message.textContent = 'scissors beats paper';
             return 'h';
         }
     }
+
 }
 
 function playGame() {
     let humanScore = 0;
     let computerScore = 0;
+    let round = 0;
 
-    for (let i = 1; i<=5; i++) {
 
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-    
-        console.log(`Your choice: ${humanSelection}`);
-        console.log(`Computer's choice: ${computerSelection}`);
-    
-        let round = playRound(humanSelection, computerSelection);
-        if (round === 'h') {
-            humanScore++;
-        } else if (round === 'c') {
-            computerScore++;
+    buttons.addEventListener("click", event => {
+        round++;
+        let choice = event.target.id;
+
+        if (round <= 5) {
+            const humanSelection = choice;
+            const computerSelection = getComputerChoice();
+        
+            console.log(`Your choice: ${humanSelection}`);
+            console.log(`Computer's choice: ${computerSelection}`);
+
+            let game = playRound(humanSelection, computerSelection);
+
+            if (game === 'h') {
+                humanScore++;
+            } else if (game === 'c') {
+                computerScore++;
+            }
+
+            console.log(round);
+            showRound.textContent = `Round: ${round}`;
+
+            scores.textContent = `${humanScore}:${computerScore}`;
+            gamePlay.textContent = `${humanSelection} vs ${computerSelection}`;
+
+        } 
+        
+        if (round == 5) {
+            showRound.textContent = '';
+
+            scores.classList.add("win");
+            message.classList.add("win");
+            if (humanScore>computerScore) {
+                message.textContent = "You are the winner";
+            } else if (humanScore<computerScore) {
+                message.textContent = "The computer has won";
+            } else {
+                message.textContent = "It's a tie";
+            }
         }
-    }
-    
-    //Announce the winner
-    if (humanScore>computerScore) {
-        console.log("You are the winner");
-    } else if (humanScore<computerScore) {
-        console.log("The computer has won");
-    } 
+
+    })
 }
 
 playGame();
